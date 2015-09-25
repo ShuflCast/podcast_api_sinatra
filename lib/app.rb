5eef3416d1f6813331c0413b6f813af14018355a
Antonio Voce <antonio.v0c3@gmail.com>
2015-09-25 10:20:02 +0100

    working api call

--- lib/app.rb
+++ lib/app.rb
@@ -1,26 +1,26 @@
 require 'sinatra/base'
-require 'audiosearch'
+require_relative 'logic'
 
 class PodcastApi < Sinatra::Base
 
-  callback = 'urn:ietf:wg:oauth:2.0:oob'
-  oauth_id = '475c328ee18addadc17e6d209b50f621294dc407f029066dfc48627807efb215'
-  oauth_secret = '6674d12de429eb0847260976661fca23325442a611f82b3343e1c0ebc8d49bcf'
-  auth_code = '825041365880b4fb245c9ba5a53e3101084c9c9568d687d05fd831984986a216'
-  url = 'https://www.audiosear.ch/api/search/episodes/filters[categories.name_lc]='
+  oauth_id = 'a7f1fe8ba909739c5fc789a8a0211c2c7efe17d89054103fce7d40443b587431'
+  oauth_secret = '7654005a9ed92b064d11715350c45474f85abe7e732b8a0daeb8d7eda8d96126'
 
   client = Audiosearch::Client.new(
-  :id     => '475c328ee18addadc17e6d209b50f621294dc407f029066dfc48627807efb215',
-  :secret => '6674d12de429eb0847260976661fca23325442a611f82b3343e1c0ebc8d49bcf',
+  :id     => oauth_id,
+  :secret => oauth_secret,
   :host   => 'https://www.audiosear.ch/',
   :debug  => false,
 )
 
   get '/search' do
-    # search_term = params[:search_term]
-    # response = client.search({q: 'test'}, 'episodes')
-    show = client.get('/shows/1')
-    p show
+    res = client.search({ q: 'story', size: 400, from: 0 })
+    if !res.is_success
+      raise "There was a problem searching: #{res.status} #{res}"
+    end
+    res.results.each do |episode|
+      printf("[%s] %s (%s)\n", episode.id, episode.title, episode.show_title)
+    end
   end
 
   # start the server if ruby file executed directly
